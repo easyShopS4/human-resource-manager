974f621e989f2510ba9d2fd3ef42c87dfbab2e6e
sevenyoungairye <849033901@qq.com>
2020-12-07 09:40:09 +0800

    感谢jiang的页面。

---
+++ js/myjs.js
@@ -0,0 +1,247 @@
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(oneAllChoose)', function (data) {
+        var child = $(".oneAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(oneOneChoose)', function (data) {
+        var child = $(".oneOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(twoOneChoose)', function (data) {
+        var child = $(".twoOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(threeOneChoose)', function (data) {
+        var child = $(".threeOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(fourOneChoose)', function (data) {
+        var child = $(".fourOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(fiveOneChoose)', function (data) {
+        var child = $(".fiveOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(sixOneChoose)', function (data) {
+        var child = $(".sixOne input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(twoAllChoose)', function (data) {
+        var child = $(".twoAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(sixTwoChoose)', function (data) {
+        var child = $(".sixTwo input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(threeAllChoose)', function (data) {
+        var child = $(".threeAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(fourAllChoose)', function (data) {
+        var child = $(".fourAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(fiveAllChoose)', function (data) {
+        var child = $(".fiveAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(sixAllChoose)', function (data) {
+        var child = $(".sixAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(oneSixChoose)', function (data) {
+        var child = $(".oneSix input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(twoSixChoose)', function (data) {
+        var child = $(".twoSix input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(threeSixChoose)', function (data) {
+        var child = $(".threeSix input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(fourSixChoose)', function (data) {
+        var child = $(".fourSix input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(sevenAllChoose)', function (data) {
+        var child = $(".sevenAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
+//============================================================================
+layui.use(['form','jquery'], function () {
+    var form = layui.form;
+    var $ = layui.jquery;
+    //点击全选, 勾选
+    form.on('checkbox(eightAllChoose)', function (data) {
+        var child = $(".eightAll input[type='checkbox']");
+        child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
+    });
+});
