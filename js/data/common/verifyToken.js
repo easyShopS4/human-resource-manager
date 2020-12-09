664f531f14f48d7285248cbc8445f0b6482ae1dc
sevenyoungairye <849033901@qq.com>
2020-12-09 21:18:21 +0800

    访问页面需要授权，后台获取当前用户数据传入header，取localStorage里面的token信息,可~

--- js/data/common/verifyToken.js
+++ js/data/common/verifyToken.js
@@ -9,25 +9,29 @@ layui.use(['layer', 'jquery', 'form'], function () {
         $ = layui.jquery
 
     let jwt_token = localStorage.getItem("jwtToken");
-    // alert("cccc"+jwt_token);
-    $.ajax({
-        url: "http://127.0.0.1:8080/ssm/config-file-first-kind/all",
-        type: "post",
-        dataType:"json",
-        /*beforeSend: function (xhr) {
-            //    //发送ajax请求之前向http的head里面加入验证信息
-            // xhr.setRequestHeader("jwtToken", "Basic "+window.btoa("dsfdf"));  // 请求发起前在头部附加token
 
-        },*/
-        headers: {
-            "jwtToken":"Access-Token123456",//自定义请求头
-            "Content-Type":"application/json;charset=utf8",
-            "abc": jwt_token
+    $.ajax({
+        url: "http://127.0.0.1:8080/ssm/users/check",
+        type: "post",
+        data: {"name": "mr.lee"},
+        dataType:"json",
+        beforeSend: function (xhr) {
+            //    //发送ajax请求之前向http的head里面加入验证信息
+            xhr.setRequestHeader("jwtToken", jwt_token);  // 请求发起前在头部附加token
+            xhr.setRequestHeader("abc", "abc")
         },
-        crossDomain: true,
         success: res => {
-            console.log(res)
-            if (!res) {
+            console.log('msg---->', res)
+
+            // 更新token
+            localStorage.removeItem("jwtToken")
+            localStorage.setItem("jwtToken", res)
+
+        },
+        error: e => {
+            // 进入error回调
+            console.log('err', e)
+            if (e.responseText == "bad") {
                 layer.msg("您无权限，请登录")
                 location.href = "../login.html";
             }
